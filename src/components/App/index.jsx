7a2628d10e0ba8e038a90317542cdb9066cb0c0f
Celso Pereira <99039824+CelsoPereiraDev@users.noreply.github.com>
2022-03-10 21:19:59 -0300

    Adição dos componentes da TaskList

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Logo from '../Logo';
 import './style.css';
 import Form from '../Form';
+import TaskList from '../TaskList';
 
 function App() {
   return (
     <div className="app">
       <Logo />
       <Form />
+      <div className="app__tasks-container">
+        <TaskList title="To-do" />
+        <TaskList title="Done" />
+      </div>
     </div>
   );
 }
--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './style.css';
+import PropTypes from 'prop-types';
 
-function TaskList() {
+function TaskList({ title }) {
   return (
     <section className="tasks-section">
-      <h2 className="tasks-section__tittle">To-do</h2>
+      <h2 className="tasks-section__tittle">{ title }</h2>
       <ul className="tasks-section__list" id="list-todo">
         <li className="tasks-section__item">
           <label className="tasks-section__item__label">
@@ -13,7 +14,7 @@ function TaskList() {
           </label>
           <button type="button" className="tasks-section__item__remove">X</button>
         </li>
-        <li className="tasks-section__item" data-id="1646952874227">
+        <li className="tasks-section__item">
           <label className="tasks-section__item__label">
             <input className="tasks-section__item__checkbox" type="checkbox" />
             <span className="tasks-section__item__text">pitbull</span>
@@ -24,5 +25,9 @@ function TaskList() {
     </section>
   );
 }
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default TaskList;
